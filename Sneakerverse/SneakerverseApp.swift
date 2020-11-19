b2224ed23f36582c23a3b07bd91b2ae89837ae99
Dung Trinh <duuung1998@gmail.com>
2020-11-19 16:30:52 +0100

    Initial Commit

---
+++ Sneakerverse/SneakerverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerverseApp.swift
+//  Sneakerverse
+//
+//  Created by Dung  on 19.11.20.
+//
+
+import SwiftUI
+
+@main
+struct SneakerverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
