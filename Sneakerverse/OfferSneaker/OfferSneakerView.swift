7d6f1c3ca1d6066b29157716fad696c1ac352225
Dung Trinh <duuung1998@gmail.com>
2020-12-10 11:11:08 +0100

    add pop up view

--- Sneakerverse/OfferSneaker/OfferSneakerView.swift
+++ Sneakerverse/OfferSneaker/OfferSneakerView.swift
@@ -11,45 +11,65 @@ struct OfferSneakerView: View {
     @ObservedObject var viewModel = OfferSneakerViewModel()
     
     @State var isValid = true
+    @State var showResponse: Bool = false
     
     var body: some View {
-        VStack{
-            Form{
-                Section{
-                    Group{
-                        Image(systemName: "plus.circle.fill")
-                        Text("add picture")
+        ZStack{
+            VStack{
+                Form{
+                    Section{
+                        Group{
+                            Image(systemName: "plus.circle.fill")
+                            Text("add picture")
+                        }
+                    }
+                    VStack{
+                        CustomTextField(storedText: $viewModel.sneakerName, placholderText: "Sneaker name",type: .TEXT,icon: Image(systemName: "person.fill"))
+                        CustomTextField(storedText: $viewModel.description, placholderText: "Description",type: .TEXT, icon: Image(systemName:"text.below.photo.fill"))
+                        CustomTextField(storedText: $viewModel.price, placholderText: "Price",type: .NUMBERS, icon: Image(systemName:"tag.fill"))
+                        
+                    }
+                    Section{
+                        SneakerDataPicker(type: DataPickerType.SIZES, selectedValue: $viewModel.size)
+                        SneakerDataPicker(type: DataPickerType.BRANDS, selectedValue: $viewModel.brand)
+                        SneakerDataPicker(type: DataPickerType.CONDITION, selectedValue: $viewModel.condition)
+                    }
+                    Button(action: {
+                        print( """
+                        name: \(viewModel.sneakerName)
+                        description: \(viewModel.description)
+                        price: \(viewModel.price)
+                        size: \(viewModel.size)
+                        brand: \(viewModel.brand)
+                        condition: \(viewModel.condition)
+                        """)
+                        
+                        viewModel.offerSneaker(sneakerOffer: SneakerOffer(sneakerName: viewModel.sneakerName, description: viewModel.description, price: viewModel.price, size: viewModel.size, brand: viewModel.size, condition: viewModel.condition), completion: {success in
+                            if (success){
+                                self.showResponse = true
+                            }
+                        })
+                        
+                    }) {
+                        CustomButton(buttonText: "add sneaker", buttonColor: .blue)
                     }
                 }
-                VStack{
-                    CustomTextField(storedText: $viewModel.sneakerName, placholderText: "Sneaker name",type: .TEXT,icon: Image(systemName: "person.fill"))
-                    CustomTextField(storedText: $viewModel.description, placholderText: "Description",type: .TEXT, icon: Image(systemName:"text.below.photo.fill"))
-                    CustomTextField(storedText: $viewModel.price, placholderText: "Price",type: .NUMBERS, icon: Image(systemName:"tag.fill"))
-                    
-                }
-                Section{
-                    SneakerDataPicker(type: DataPickerType.SIZES, selectedValue: $viewModel.size)
-                    SneakerDataPicker(type: DataPickerType.BRANDS, selectedValue: $viewModel.brand)
-                    SneakerDataPicker(type: DataPickerType.CONDITION, selectedValue: $viewModel.condition)
-                }
+                
+                
+                
+                
+                
             }
             
-            Button(action: {
-                print( """
-                    name: \(viewModel.sneakerName)
-                    description: \(viewModel.description)
-                    price: \(viewModel.price)
-                    size: \(viewModel.size)
-                    brand: \(viewModel.brand)
-                    condition: \(viewModel.condition)
-                    """)
+            if showResponse {
                 
-                viewModel.offerSneaker(sneakerOffer: SneakerOffer(sneakerName: viewModel.sneakerName, description: viewModel.description, price: viewModel.price, size: viewModel.size, brand: viewModel.size, condition: viewModel.condition), completion: {success in
-                })
-            }) {
-                CustomButton(buttonText: "add sneaker", buttonColor: .blue)
+                PopUpView(show: $showResponse)
             }
         }
+        .padding()
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
---
+++ Sneakerverse/base/viewObjects/PopUpView.swift
@@ -0,0 +1,64 @@
+//
+//  PopUpView.swift
+//  Sneakerverse
+//
+//  Created by Dung  on 09.12.20.
+//
+
+import SwiftUI
+
+struct PopUpView: View {
+    @Binding var show : Bool
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)){
+            VStack(spacing: 25){
+                Image(systemName: "photo.fill")
+                
+                Text("Congratulation!")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                
+                Text("you have successfully offered your sneaker!")
+                
+                Button(action: {}) {
+                    Text("Back to Home")
+                }
+                
+            }
+            .padding(.vertical,25)
+            .padding(.horizontal,25)
+            .background(BlurView())
+            .cornerRadius(25)
+            
+            Button(action: {
+                show.toggle()
+            }){
+                Image(systemName: "xmark.circle.fill")
+                    .font(.system(size: 28,weight: .bold))
+                    .foregroundColor(.blue)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+    }
+}
+
+//struct PopUpView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PopUpView()
+//    }
+//}
+
+
+struct BlurView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIVisualEffectView{
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
+}
