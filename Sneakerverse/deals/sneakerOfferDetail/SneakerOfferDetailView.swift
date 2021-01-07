6603f8f0a88e328ea5d5ea9bf1268671ee45cd61
Hoang Ha Vu <hongiibongii@gmail.com>
2021-01-07 19:28:00 +0100

    added sneakerofferdetail files

---
+++ Sneakerverse/deals/sneakerOfferDetail/SneakerOfferDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SneakerOfferDetailView.swift
+//  Sneakerverse
+//
+//  Created by Hoang Ha Vu on 07.01.21.
+//
+
+import SwiftUI
+
+struct SneakerOfferDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SneakerOfferDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SneakerOfferDetailView()
+    }
+}
---
+++ Sneakerverse/deals/sneakerOfferDetail/SneakerOfferDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SneakerOfferDetailViewModel.swift
+//  Sneakerverse
+//
+//  Created by Hoang Ha Vu on 07.01.21.
+//
+
+import Foundation
