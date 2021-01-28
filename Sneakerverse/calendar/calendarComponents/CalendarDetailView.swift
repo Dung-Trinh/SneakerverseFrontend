252eae60a615ced77a8a1daed5d7fbbc4fd0d2a1
Dung Trinh <duuung1998@gmail.com>
2021-01-28 13:32:26 +0100

    added calendar detail view

---
+++ Sneakerverse/calendar/calendarComponents/CalendarDetailView.swift
@@ -0,0 +1,76 @@
+//
+//  CalendarDetailView.swift
+//  Sneakerverse
+//
+//  Created by Dung  on 28.01.21.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct CalendarDetailView: View {
+    var sneaker: SneakerCalendarItem
+    
+    var body: some View {
+        ScrollView{
+            KFImage.url(URL(string: sneaker.imageURL))
+                .loadDiskFileSynchronously()
+                .placeholder{return Image("default-calendar-sneaker")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 200)}
+                .cacheMemoryOnly()
+                .onProgress { receivedSize, totalSize in  }
+                .onSuccess { result in  }
+                .onFailure { error in }
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 200)
+            
+            Text(sneaker.title)
+                .font(.title2)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Text(sneaker.releaseCalendarDescription)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            VStack{
+                HStack{
+                    VStack(alignment: .trailing){
+                        Text("Preis:").fontWeight(.bold)
+                        Text("Spanne").fontWeight(.bold)
+                    }
+                    .padding(.leading)
+                    VStack(alignment: .leading){
+                        Text(sneaker.retailPrice + " €")
+                        Text(sneaker.priceSpan + " €")
+                    }
+                    .padding(.trailing)
+                    .lineLimit(1)
+                    .frame(maxWidth: 200)
+                }
+            }.padding()
+            
+            ForEach(sneaker.imgArray , id: \.self){ img in
+                KFImage.url(URL(string: img))
+                    .loadDiskFileSynchronously()
+                    .cacheMemoryOnly()
+                    .onProgress { receivedSize, totalSize in  }
+                    .onSuccess { result in  }
+                    .onFailure { error in }
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: .infinity, height: .infinity)
+            }
+        }
+    }
+}
+
+struct CalendarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarDetailView(sneaker: SneakerCalendarItem(title: "adidas Yeezy Boost 350 V2 Black Red", imageURL: "", retailPrice: "100", priceSpan: "100-200", releaseDate: "11.11.20", imgArray: [], brand: "NIKE", releaseCalendarDescription: "beschreibung", position: ""))
+    }
+}
--- Sneakerverse/calendar/calendarComponents/SneakerPreview.swift
+++ Sneakerverse/calendar/calendarComponents/SneakerPreview.swift
@@ -11,40 +11,43 @@ import Kingfisher
 struct SneakerPreview: View {
     var sneaker: SneakerCalendarItem
     var body: some View {
-        ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
-            KFImage.url(URL(string: self.sneaker.imageURL))
-                .loadDiskFileSynchronously()
-                .placeholder{return Image("default-calendar-sneaker")
+        NavigationLink(
+            destination: CalendarDetailView(sneaker: sneaker)){
+            ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
+                KFImage.url(URL(string: self.sneaker.imageURL))
+                    .loadDiskFileSynchronously()
+                    .placeholder{return Image("default-calendar-sneaker")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 200)}
+                    .cacheMemoryOnly()
+                    .onProgress { receivedSize, totalSize in  }
+                    .onSuccess { result in  }
+                    .onFailure { error in }
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 150, height: 200)}
-                .cacheMemoryOnly()
-                .onProgress { receivedSize, totalSize in  }
-                .onSuccess { result in  }
-                .onFailure { error in }
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 200)
-            
-            VStack(alignment: .leading, spacing: 30){
-                Text(sneaker.releaseDate.prefix(2))
-                    .font(.title)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(Color.black)
-                    .padding(.top,-80)
-                    .padding(.leading,12)
+                    .frame(width: 150, height: 200)
                 
-                Text(sneaker.title)
-                    .font(.system(size: 13))
-                    .foregroundColor(Color.black)
-                    .multilineTextAlignment(.center)
-                    .padding(12)
-                    .frame(width: 150, height:80)
+                VStack(alignment: .leading, spacing: 30){
+                    Text(sneaker.releaseDate.prefix(2))
+                        .font(.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(Color.black)
+                        .padding(.top,-80)
+                        .padding(.leading,12)
+                    
+                    Text(sneaker.title)
+                        .font(.system(size: 13))
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.center)
+                        .padding(12)
+                        .frame(width: 150, height:80)
+                }
+                
             }
-            
+            .shadow(radius: 20 )
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
         }
-        .shadow(radius: 20 )
-        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
     }
 }
 
