73196d16129342b084faf6256a84833089e7906a
Hoang Ha Vu <hongiibongii@gmail.com>
2021-02-11 00:31:49 +0100

    started working on login in user after signup

--- Sneakerverse/signup/SignUpView.swift
+++ Sneakerverse/signup/SignUpView.swift
@@ -19,27 +19,32 @@ struct SignUpView: View {
     
     var body: some View {
         NavigationView{
-            VStack {
-                Title(text:"Registrieren")
-                
-                CustomTextField(storedText: $signUpViewModel.username, placholderText: "Username", type: .TEXT, icon: Image(systemName: "person.fill"))
-                CustomTextField(storedText: $signUpViewModel.password, placholderText: "Password", type: .PASSWORD)
-                CustomTextField(storedText: $signUpViewModel.passwordVerify, placholderText: "Password wiederholen", type: .PASSWORD)
-                
-                ResponseSignUp(message: $signUpViewModel.responseMessage)
-                
-                Button(action: {
-                    if(signUpViewModel.checkPasswordsAreTheSame(firstPW: signUpViewModel.password, secondPW: signUpViewModel.passwordVerify)){
-                        
-                        signUpViewModel.signUp(username: signUpViewModel.username, password: signUpViewModel.password, completion: { response in
-                            signUpViewModel.responseMessage = response ? SignUpStates.SUCCESSFUL : SignUpStates.SIGNUPFAILED
-                        })
-                    }else{
-                        signUpViewModel.responseMessage = SignUpStates.PASSWORDFAILED
+            if(!signUpViewModel.navigateToHome){
+                VStack {
+                    Title(text:"Registrieren")
+                    
+                    CustomTextField(storedText: $signUpViewModel.username, placholderText: "Username", type: .TEXT, icon: Image(systemName: "person.fill"))
+                    CustomTextField(storedText: $signUpViewModel.password, placholderText: "Password", type: .PASSWORD)
+                    CustomTextField(storedText: $signUpViewModel.passwordVerify, placholderText: "Password wiederholen", type: .PASSWORD)
+                    
+                    ResponseSignUp(message: $signUpViewModel.responseMessage)
+                    
+                    Button(action: {
+                        if(signUpViewModel.checkPasswordsAreTheSame(firstPW: signUpViewModel.password, secondPW: signUpViewModel.passwordVerify)){
+                            
+                            signUpViewModel.signUp(username: signUpViewModel.username, password: signUpViewModel.password, completion: { response in
+                                signUpViewModel.responseMessage = response ? SignUpStates.SUCCESSFUL : SignUpStates.SIGNUPFAILED
+                            })
+                        }else{
+                            signUpViewModel.responseMessage = SignUpStates.PASSWORDFAILED
+                        }
+                    }){
+                        CustomButton(buttonText: "Sign Up", buttonColor: .blue)
                     }
-                }){
-                    CustomButton(buttonText: "Sign Up", buttonColor: .blue)
                 }
+            }else{
+                NavigationLink("toHomeView", destination: HomeView(),
+                               isActive: $signUpViewModel.navigateToHome)
             }
         }
     }
