b82aa09c076f7b51a66eda32bcfb48fd6ff107c5
Hoang Ha Vu <hongiibongii@gmail.com>
2021-02-18 23:59:08 +0100

    finallfy adjusted the design for BlogPostListItemView

--- Sneakerverse/blogPosts/blogPostListItem/BlogPostListItemView.swift
+++ Sneakerverse/blogPosts/blogPostListItem/BlogPostListItemView.swift
@@ -14,36 +14,46 @@ struct BlogPostListItemView: View {
         ZStack{
             Rectangle()
                 .foregroundColor(.red)
-                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 0, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
             VStack{
                 KFImage.url(URL(string: self.blog.cover))
                     .loadDiskFileSynchronously()
                     .placeholder{return Image("default-calendar-sneaker")
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 150, height: 200)}
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 170)}
                     .cacheMemoryOnly()
                     .onProgress { receivedSize, totalSize in  }
                     .onSuccess { result in  }
                     .onFailure { error in }
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
                     .frame(width: .infinity, height: 180)
+                    .scaledToFit()
                 HStack{
                     Text(blog.title)
-                        .padding(.leading,50)
+                        .foregroundColor(Color.black)
+                        .fontWeight(.semibold)
+                        .padding(.leading,20)
                     Spacer()
-                }
+                }.padding(.bottom,5)
                 HStack {
                     Text(blog.blogDescription)
-                        .padding(.leading,50)
+                        .font(.system(size:14))
+                        .foregroundColor(Color.black)
+                        .padding(.leading,20)
                     Spacer()
-                }
+                }.padding(.bottom,5)
                 HStack {
                     Spacer()
                     Text("weiterlesen")
-                        .padding(.trailing,50)
-                }
+                        .foregroundColor(Color.black)
+                    Image(systemName:"arrow.right.circle.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .padding(.trailing,20)
+                        
+                        .foregroundColor(Color.black)
+                }.padding(.bottom,10)
             }
         }.frame(width: .infinity, height: 300)
         .cornerRadius(20)
