6569c34dd685a10931a41cf33aca5d1696c61b16
Hoang Ha Vu <hongiibongii@gmail.com>
2021-03-02 15:14:29 +0100

    adjusted DealsListItemView

--- Sneakerverse/deals/overviewListItem/DealsOverviewListItemView.swift
+++ Sneakerverse/deals/overviewListItem/DealsOverviewListItemView.swift
@@ -17,19 +17,22 @@ struct DealsOverviewListItemView: View {
             VStack {
                 Image("default-sneaker")
                     .resizable()
-                    .frame(width: 110, height: 120)
-                    .offset(x: -10, y: 0)
+                    .frame(width: 110, height: 110)
                 Text(offer.city.cityName)
-                        .offset(x: 0, y: -10)
-                        .foregroundColor(.gray)
+                        .font(.system(size:15))
+                    .foregroundColor(DESIGN.COLOR.darkPurple)
+                Text(offer.name)
+                    .font(.system(size:15))
+                    .foregroundColor(.black)
+                    .fontWeight(.semibold)
+                    .padding(.bottom,5)
                 HStack {
-                    Text(offer.name)
-                        .font(.system(size:20))
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     Spacer()
-                    Text("\(offer.price) $")
-                        .bold()
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text("\(offer.price, specifier: "%.2f") $")
+                        .font(.system(size:15))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .padding(.bottom,5)
                 }.padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             }
         }.frame(width: 180, height: 180)
@@ -37,9 +40,9 @@ struct DealsOverviewListItemView: View {
     }
 }
 
-//struct DealsOverviewItemListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        DealsOverviewListItemView(offer: Offer(id: "123123", name: "Test Sneaker", offerDescription: "testi description", price: 30, size: 8, brand: "Nike", condition: "new", ownerName: "Hoang", ownerID: "123", city: City(id: "asdasda", latitude: 8.6, longitude: 9.0, cityName: "Wiesbaden"), created: "2021-01-06T19:22:16.459Z", img: <#[ImgElement]#>, v: 0))
-//            .previewLayout(.sizeThatFits)
-//    }
-//}
+struct DealsOverviewItemListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DealsOverviewListItemView(offer: Offer(id: "123123", name: "Test Sneaker", offerDescription: "testi description", price: 30, size: 8, brand: "Nike", condition: "new", ownerName: "Hoang", ownerID: "123", city: City(id: "asdasda", latitude: 8.6, longitude: 9.0, cityName: "Wiesbaden"), created: "2021-01-06T19:22:16.459Z", v: 0))
+            .previewLayout(.sizeThatFits)
+    }
+}
